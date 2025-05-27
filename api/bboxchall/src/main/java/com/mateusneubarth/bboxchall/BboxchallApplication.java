fe65ff93aa8fea99970ae53b9a17852f4d1214d8
Mateus Neubarth <mateusneubarth@hotmail.com>
2025-05-27 10:49:51 -0300

    back-end setup commit

---
+++ api/bboxchall/src/main/java/com/mateusneubarth/bboxchall/BboxchallApplication.java
@@ -0,0 +1,13 @@
+package com.mateusneubarth.bboxchall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BboxchallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BboxchallApplication.class, args);
+	}
+
+}
---
+++ api/bboxchall/src/test/java/com/mateusneubarth/bboxchall/BboxchallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateusneubarth.bboxchall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BboxchallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
